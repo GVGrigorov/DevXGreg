c9ddec5faaa78674aa5077d30d1438ee924e8467
GVGrigorov <greg.v.grigorov@gmail.com>
2022-11-04 20:12:23 -0500

    "I Com Mit Ted A Gain"

--- src/TestApp.java
+++ src/TestApp.java
@@ -1,8 +1,9 @@
-public class Main {
+public class TestApp {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Git Commit Ted");
         System.out.println("Git Get Go");
         System.out.println("");
+        System.out.println("More Git Get Go In A Row");
     }
 }
